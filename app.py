258977d4b72f254dffc85887eb3c8a812f2adec7
Ortizyasociados <zonasmith@gmail.com>
2026-06-30 00:31:08 -0500

    Update app.py

--- app.py
+++ app.py
@@ -26,8 +26,8 @@ st.markdown("""
 # Usamos el sidebar nativo de Streamlit para el control, es más limpio
 with st.sidebar:
    st.markdown('<div style="font-family: sans-serif; font-size: 20px; font-weight: bold; color: white;">ORTIZ Y ASOCIADOS</div>', unsafe_allow_html=True)
-    st.write("Panel de Operaciones")
-    uploaded_files = st.file_uploader("Cargar ZIP de facturas", type=["zip"])
+   st.write("Panel de Operaciones")
+   uploaded_files = st.file_uploader("Cargar ZIP de facturas", type=["zip"])
     
     # EL BOTÓN DE TERCEROS BIEN POSICIONADO
     if st.button("Ver Terceros Registrados"):
